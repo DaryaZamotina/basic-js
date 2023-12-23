b9a83f196c2839766aa8a7786a6ead91bf520f6f
Darya Zamotina <133055781+DaryaZamotina@users.noreply.github.com>
2023-12-23 18:53:36 +0300

    feat: repeater done

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,46 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let [
+    repeatTimes, 
+    separator, 
+    addition, 
+    additionRepeatTimes, 
+    additionSeparator,
+    ] = Object.keys(options);
+  if (typeof str != String) {
+    str = String(str);
+  }
+  
+  if (typeof options[addition] != String) {
+    options[addition] = String(options[addition])
+  }
+
+    let strAdd = (options[addition].concat(options[additionSeparator])).repeat(options[additionRepeatTimes]);
+    let strAddStrict = strAdd.substring(0, strAdd.length-2);
+    let str1 = str.concat(strAddStrict).concat(options[separator]);
+    let str2 = str1.repeat(options[repeatTimes]);
+    
+    if (typeof options[addition] == undefined) {
+      str1 = str.concat(options[separator]);
+    }
+    if (typeof options[repeatTimes] == undefined) {
+      options[repeatTimes] = 1;
+    }
+    if (typeof options[separator] == undefined) {
+      options[separator] = '+';
+    }
+    if (typeof options[additionRepeatTimes] == undefined) {
+      options[additionRepeatTimes] = 1;
+    }
+    if (typeof options[additionSeparator] == undefined) {
+      options[additionSeparator] = '|';
+    }
+
+    let res = str2.substring(0, str2.length-2);
+
+    return res;
 }
 
 module.exports = {
