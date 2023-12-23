f691a59d70f783161ba0dc7682082e8ef92ca052
Darya Zamotina <133055781+DaryaZamotina@users.noreply.github.com>
2023-12-23 22:18:03 +0300

    feat: repeater done

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -16,46 +16,52 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function repeater(str, options) {
-  let [
-    repeatTimes, 
-    separator, 
-    addition, 
-    additionRepeatTimes, 
-    additionSeparator,
-    ] = Object.keys(options);
-  if (typeof str != String) {
-    str = String(str);
+	if (!options) {
+		return str;
+	}
+	let [
+		repeatTimes, 
+		separator, 
+		addition, 
+		additionRepeatTimes, 
+		additionSeparator,
+		] = Object.keys(options);
+	
+	if (typeof str != "String") {
+		str = String(str);
+	  }
+	  
+	  if (!options[repeatTimes]) {
+		options[repeatTimes] = 1;
+	  }
+	  if (typeof options[separator] === "undefined") {
+		options[separator] = '+';
+	  }
+	  if (typeof options[additionRepeatTimes] === "undefined") {
+		options[additionRepeatTimes] = 1;
+	  }
+	  if (typeof options[additionSeparator] === "undefined") {
+		options[additionSeparator] = '|';
+	  }
+	  
+	  if (typeof options[addition] !== "string" || typeof options[additionSeparator] !== "string" || typeof options[separator] !== "string") {
+      options[addition] = String(options[addition]);
+      options[additionSeparator] = String(options[additionSeparator]);
+      options[separator] = String(options[separator]);
+	  }
+	  
+	let strAdd = (options[addition] + options[additionSeparator]).repeat(options[additionRepeatTimes]);
+	let strAddStrict = strAdd.substring(0, strAdd.length-options[additionSeparator].length);
+	let str1 = str + strAddStrict + options[separator];
+	
+	if (typeof options[addition] == "undefined") {
+		str1 = str + options[separator];
+	  }
+ 	
+	let str2 = str1.repeat(options[repeatTimes]);
+	let res = str2.substring(0, str2.length-options[separator].length);
+ 	return res;
   }
-  
-  if (typeof options[addition] != String) {
-    options[addition] = String(options[addition])
-  }
-
-    let strAdd = (options[addition].concat(options[additionSeparator])).repeat(options[additionRepeatTimes]);
-    let strAddStrict = strAdd.substring(0, strAdd.length-2);
-    let str1 = str.concat(strAddStrict).concat(options[separator]);
-    let str2 = str1.repeat(options[repeatTimes]);
-    
-    if (typeof options[addition] == undefined) {
-      str1 = str.concat(options[separator]);
-    }
-    if (typeof options[repeatTimes] == undefined) {
-      options[repeatTimes] = 1;
-    }
-    if (typeof options[separator] == undefined) {
-      options[separator] = '+';
-    }
-    if (typeof options[additionRepeatTimes] == undefined) {
-      options[additionRepeatTimes] = 1;
-    }
-    if (typeof options[additionSeparator] == undefined) {
-      options[additionSeparator] = '|';
-    }
-
-    let res = str2.substring(0, str2.length-2);
-
-    return res;
-}
 
 module.exports = {
   repeater
